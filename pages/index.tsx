5cd8b71b5eb3f3825c6c2fd3f6300121de35c5f6
dong-park <rakddh@gmail.com>
2022-06-01 20:56:11 +0900

    4.6 Modifiers for Lists

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,20 +8,30 @@ const Home: NextPage = () => {
         <div className='bg-slate-400 py-20 px-20 grid gap-10 min-h-screen'>
             <div className='bg-white p-10 rounded-3xl shadow-xl'>
                 <span className="font-semibold text-2xl">Select Item</span>
-                <div className='flex justify-between my-2'>
-                    <span className='text-gray-500'>Grey Chair</span>
-                    <span className='font-semibold'>$19</span>
-                </div>
-                <div className='flex justify-between'>
-                    <span className='text-gray-500'>Grey Chair</span>
-                    <span>$19</span>
-                </div>
+                <ul>
+                    {[1, 2, 3, 4].map((i) => (
+                        <div key={i} className='flex justify-between my-2 odd:bg-blue-50 even:bg-yellow-500'>
+                            <span className='text-gray-500'>Grey Chair</span>
+                            <span className='font-semibold'>$19</span>
+                        </div>
+                    ))}
+                </ul>
+                <ul>
+                    {["a", "b", "c"].map((c, i) => (
+                        <li className='bg-red-500'>
+                            {c}
+                        </li>
+                    ))}
+                </ul>
                 <div className='flex justify-between mt-2 pt-2 border-t-2 border-dashed'>
                     <span>Total</span>
                     <span className="font-semibold">$10</span>
                 </div>
                 <div
-                    className='mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto'
+                    className="
+                    mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto
+                    hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500
+                    "
                 >
                     Checkout
                 </div>
@@ -54,8 +64,8 @@ const Home: NextPage = () => {
                 <div className='flex mb-5 justify-between items-center'>
                     <span>⬅️</span>
                     <div className='space-x-3'>
-                        <span>★ 4.9</span>
-                        <span>♥</span>
+                        <span>⭐️ 4.9</span>
+                        <span className="shadow-xl p-2 rounded-md">💖</span>
                     </div>
                 </div>
                 <div className='bg-zinc-400 h-72 mb-5'></div>
@@ -63,10 +73,10 @@ const Home: NextPage = () => {
                     <span className='font-medium text-xl'>Swoon Lounge</span>
                     <span className='text-xs text-gray-500'>Chair</span>
                     <div className='mt-3 mb-5 flex justify-between items-center'>
-                        <div>
-                            <input type="radio" />
-                            <input type="radio" />
-                            <input type="radio" />
+                        <div className='space-x-2'>
+                            <button className='w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition' />
+                            <button className='w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition' />
+                            <button className='w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition' />
                         </div>
                         <div className='flex items-center space-x-5'>
                             <button className='rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500'>-</button>
